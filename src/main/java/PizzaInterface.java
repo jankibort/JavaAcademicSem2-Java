02e6fb5d7b14655ebbabd875cae1dbd4ceb9e5fe
wini <imbatman@wp.pl>
2019-06-08 23:52:57 +0200

    dodanie interface

---
+++ src/main/java/PizzaInterface.java
@@ -0,0 +1,8 @@
+import builders.PizzaBuilder;
+
+public interface PizzaInterface {
+    public PizzaBuilder setName();
+    public PizzaBuilder setIngredients();
+    public PizzaBuilder setSpiciness();
+    public PizzaBuilder setPizza();
+}
